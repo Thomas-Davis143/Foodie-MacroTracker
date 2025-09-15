9ce9e48e92b091824d91bac0dc7c8b9e3dfd967f
Thomas <Tommygd2@gmail.com>
2025-09-15 19:56:42 -0400

    Version 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.TDavis.foodie_macrotracker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField( "String", "FOOD_PROXY_BASE_URL", "\"https://foodie-macrotracker.onrender.com\"")
